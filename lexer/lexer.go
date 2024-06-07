df4092d61b0f8e3e5bd76ab73fe359b403125a1a
靳泽旭 <jackjin@stellarrover.com>
2024-06-07 15:28:53 +0800

    perf

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -14,3 +14,19 @@ func New(input string) *Lexer {
     l := &Lexer{input: input}
     return l
 }
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+			l.ch = 0 // 0是NUL字符的ASCII编码
+	} else {
+			l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
