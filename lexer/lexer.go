49e0dd3679748ed6a10f307afc872846263dd6a1
靳泽旭 <jackjin@stellarrover.com>
2024-12-12 17:37:24 +0800

    refactor: lexer
    Co-authored-by: jack
    1037461232@qq.com
    
    Co-authored-by: NAME jackjin
    Co-authored-by: ANOTHER-NAME 1037461232@qq.com

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,6 @@
 // 词法分析器, 将源代码作为输入，并输出对应的词法单元
 // 在生产环境中，应该将文件名和行号附加到词法单元中，以便更好地跟踪可能出现的词法分析错误和语法分析错误。
+import "monkey/token"
 
 package lexer
 
@@ -29,4 +30,15 @@ func New(input string) *Lexer {
 	l := &Lexer{input: input}
 	l.readChar()
 	return l
+}
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+			l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
